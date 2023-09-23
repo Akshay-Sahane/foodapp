c15f96d7d2303d65fe5cce97f6a5dc779e6041e6
Akshay Sahane <sahanea55@gmail.com>
2023-09-23 19:23:08 +0530

    update formatting

--- myproject/food/models.py
+++ myproject/food/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Food(models.Model):
-    foodId=models.AutoField(primary_key=True)
-    foodName=models.CharField(max_length=100)
-    foodType=models.CharField(max_length=20)
-    foodPrice=models.FloatField()
-    foodDescription=models.TextField()
-    foodImage=models.ImageField(upload_to="foodimages/",default="No-Image.png")
+    foodId = models.AutoField(primary_key=True)
+    foodName = models.CharField(max_length=100)
+    foodType = models.CharField(max_length=20)
+    foodPrice = models.FloatField()
+    foodDescription = models.TextField()
+    foodImage = models.URLField()
 
     def __str__(self):
         # return f"{self.foodName}-{self.foodType}"
